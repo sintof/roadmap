dad07d59ccc5d724408f70537c5b60125b55a350
sintof <dcpersonf@gmail.com>
2024-10-04 16:35:42 +0500

    init project roadmap

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<template>
+  <app-header/>
+  <router-view/>
+</template>
+
+<script setup>
+import AppHeader from './components/AppHeader.vue'
+</script>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,13 @@
+<template>
+    <header class="bg-white flex justify-between items-center py-4 border-b border-gray-200 sm:px-6 lg:px-8 shadow">
+        <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+            <h1 class="text-3xl font-bold leading-tight text-gray-900">
+                Roadmap
+            </h1>
+        </div>
+        <nav class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+            <RouterLink class="mr-4 text-gray-900 hover:text-gray-500 focus:text-gray-700 font-medium text-lg" to="/">Home</RouterLink>
+            <RouterLink class="mr-4 text-gray-900 hover:text-gray-500 focus:text-gray-700 font-medium text-lg" to="/about">About</RouterLink>
+        </nav>
+    </header>
+</template>
